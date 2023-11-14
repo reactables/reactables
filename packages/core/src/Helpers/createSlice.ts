ee106390cbdfb86871ccc816be09ec15e3510922
David Lai <laidav@gmail.com>
2023-11-14 18:32:59 -0500

    implement create slice

--- packages/core/src/Helpers/createSlice.ts
+++ packages/core/src/Helpers/createSlice.ts
@@ -2,8 +2,10 @@ import { Reducer, Action } from '../Models';
 
 type SingleActionReducer<T, S> = (state: T, action: Action<S>) => T;
 
+type ActionCreator = (payload: unknown) => Action<unknown>;
+
 export interface Slice<T> {
-  actions: { [key: string]: Action };
+  actions: { [key: string]: ActionCreator };
   reducer: Reducer<T>;
 }
 
@@ -15,10 +17,10 @@ export interface SliceConfig<T> {
   };
 }
 
-export const createSlice = <T>(config: SliceConfig<T>) => {
+export const createSlice = <T>(config: SliceConfig<T>): Slice<T> => {
   const { name, initialState, cases } = config;
 
-  const finalReducer: Reducer<T> = Object.entries(cases).reduce(
+  const reducer: Reducer<T> = Object.entries(cases).reduce(
     (acc, [key, _case]): Reducer<T> => {
       const newFunc = <S>(state: T, action: Action<S>) => {
         if (action.type === `${name}/${key}`) {
@@ -33,5 +35,21 @@ export const createSlice = <T>(config: SliceConfig<T>) => {
     (state = initialState) => state,
   );
 
-  return finalReducer;
+  const actions: { [key: string]: ActionCreator } = Object.entries(
+    cases,
+  ).reduce(
+    <S>(
+      acc: { [key: string]: ActionCreator },
+      [key]: [string, SingleActionReducer<T, S>],
+    ) => {
+      acc[key] = (payload: S) => ({ type: `${name}/${key}`, payload });
+      return acc;
+    },
+    {},
+  );
+
+  return {
+    reducer,
+    actions,
+  };
 };
