b90caf5bc91fbb1d949413e3f840f20d0c515d68
David Lai <laidav@gmail.com>
2023-11-14 21:11:41 -0500

    update counter example

--- packages/examples/src/Counter/Counter.ts
+++ packages/examples/src/Counter/Counter.ts
@@ -1,32 +1,25 @@
-import { Reducer, Action, HubFactory, Reactable } from '@hub-fx/core';
+import { HubFactory, Reactable, createSlice } from '@hub-fx/core';
 
-// Actions
-const INCREMENT = 'INCREMENT';
-const increment = (): Action => ({ type: INCREMENT });
-
-const RESET = 'RESET';
-const reset = (): Action => ({ type: RESET });
-
-// Reducer function to handle state updates
 interface CounterState {
   count: number;
 }
-
-const reducer: Reducer<CounterState> = (state = { count: 0 }, action) => {
-  switch (action?.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case RESET:
-      return { count: 0 };
-    default:
-      return state;
-  }
-};
 
 interface CounterActions {
   increment: () => void;
   reset: () => void;
 }
+
+const {
+  reducer,
+  actions: { increment, reset },
+} = createSlice({
+  name: 'counter',
+  initialState: { count: 0 } as CounterState,
+  cases: {
+    increment: (state) => ({ count: state.count + 1 }),
+    reset: () => ({ count: 0 }),
+  },
+});
 
 export const Counter = (): Reactable<CounterState, CounterActions> => {
   const hub = HubFactory();
