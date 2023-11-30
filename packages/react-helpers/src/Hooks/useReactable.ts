9d4d3774cceb91d5bf73c39537177b60b67d564c
David Lai <laidav@gmail.com>
2023-11-29 21:10:27 -0500

    fix type

--- packages/react-helpers/src/Hooks/useReactable.ts
+++ packages/react-helpers/src/Hooks/useReactable.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
-import { Reactable, ActionMap } from '@reactables/core';
+import { Reactable } from '@reactables/core';
 
 // React Strict Mode has bugs with clean up with refs so it breaks the useReactable hook as of now
 // See Bug: https://github.com/facebook/react/issues/26315
 // See Bug: https://github.com/facebook/react/issues/24670
 
-export const useReactable = <T, S extends ActionMap>(reactable: Reactable<T, S>) => {
+export const useReactable = <T, S>(reactable: Reactable<T, S>) => {
   const { state$, actions } = useRef(reactable).current;
   const [state, setState] = useState<T>();
 
