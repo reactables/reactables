31f0f88788d29f025095670f4b48357bb1727a34
David Lai <laidav@gmail.com>
2024-03-17 20:44:17 -0400

    Revert "[82] include diff in debug"
    This reverts commit ab02e5ef23e0eb54df893e97673515c34e190310.

--- packages/core/src/Factories/HubFactory.ts
+++ packages/core/src/Factories/HubFactory.ts
@@ -4,7 +4,6 @@ import { filter, tap, map, mergeAll, scan, pairwise, startWith } from 'rxjs/oper
 import { Action } from '../Models/Action';
 import { share, shareReplay } from 'rxjs/operators';
 import { Effect } from '../Models/Effect';
-import { diff } from 'json-diff';
 
 const getScopedEffectSignature = (actionType: string, key: string | number) =>
   `type: ${actionType}, scoped: true${key ? `,key:${key}` : ''}`;
@@ -65,15 +64,19 @@ export const HubFactory = ({ effects, sources = [] }: HubConfig = {}): Hub => {
 
     const state$ = messages$.pipe(
       tap((action) => {
-        debug && console.log(debugName, '[Action]', action);
+        debug && console.log(debugName, '[Message Received]', action);
       }),
       scan(reducer, seedState),
       startWith(null, seedState),
       pairwise(),
       tap(([prevState, newState]) => {
         if (debug) {
-          console.log(debugName, '[State] State:', newState);
-          console.log(debugName, '[Diff]:', diff(prevState, newState));
+          const hasDiff = prevState !== newState;
+          if (hasDiff) {
+            console.log(debugName, '[State changed] State:', newState);
+          } else {
+            console.log(debugName, '[State unchanged] State:', newState);
+          }
         }
       }),
       map((pair) => pair[1] as T),
