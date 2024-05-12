6f545b98880937c3c926d9cf198de3baf2dd978a
David Lai <laidav@gmail.com>
2024-05-12 11:26:09 -0400

    [86] store value wrapper

---
+++ packages/core/src/Helpers/storeValue.ts
@@ -0,0 +1,18 @@
+import { ReplaySubject } from 'rxjs';
+import { Reactable } from '../Models';
+
+export interface DestroyAction {
+  destroy: () => void;
+}
+
+export const storeValue = <T, S>(reactable: Reactable<T, S>): Reactable<T, S & DestroyAction> => {
+  const replaySubject$ = new ReplaySubject<T>(1);
+
+  const [state$, actions] = reactable;
+
+  const subscription = state$.subscribe((state) => replaySubject$.next(state));
+
+  const destroy = () => subscription.unsubscribe();
+
+  return [replaySubject$, { ...actions, destroy }];
+};
