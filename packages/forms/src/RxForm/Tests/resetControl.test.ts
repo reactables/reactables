fb84b51b6daa6ecc28b6a5e45ec84df8aa0b0e06
David Lai <laidav@gmail.com>
2024-06-27 11:58:22 -0400

    update test

--- packages/forms/src/RxForm/Tests/resetControl.test.ts
+++ packages/forms/src/RxForm/Tests/resetControl.test.ts
@@ -123,89 +123,163 @@ describe('RxForm', () => {
       });
     });
 
-    // it('should emit async validation when resetting a control', () => {
-    //   testScheduler.run(({ expectObservable, cold }) => {
-    //     const [state$, { resetControl }] = build(
-    //       group({
-    //         ...asyncConfig,
-    //         controls: {
-    //           ...asyncConfig.controls,
-    //           emergencyContacts: {
-    //             ...asyncConfig.controls.emergencyContacts,
-    //             controls: asyncEmergencyContactConfigs,
-    //           },
-    //         },
-    //       }),
-    //       {
-    //         providers: { validators: Validators, asyncValidators: AsyncValidators },
-    //       },
-    //     );
+    it('should emit async validation when resetting a control and value changed', () => {
+      testScheduler.run(({ expectObservable, cold }) => {
+        const [state$, { updateValues, resetControl }] = build(
+          group({
+            ...asyncConfig,
+            controls: {
+              ...asyncConfig.controls,
+              emergencyContacts: {
+                ...asyncConfig.controls.emergencyContacts,
+                controls: asyncEmergencyContactConfigs,
+              },
+            },
+          }),
+          {
+            providers: { validators: Validators, asyncValidators: AsyncValidators },
+          },
+        );
 
-    //     subscription = cold('-b', { b: resetControl }).subscribe((resetControl) =>
-    //       resetControl(['emergencyContacts', 0]),
-    //     );
+        subscription = cold('-b 500ms c', {
+          b: () => {
+            updateValues({
+              controlRef: ['emergencyContacts', 0],
+              value: {
+                firstName: 'Sideshow',
+                lastName: 'Bob',
+                email: 'bob@sideshow.com',
+                relation: 'bad friend',
+              },
+            });
+          },
+          c: () => {
+            resetControl(['emergencyContacts', 0]);
+          },
+        }).subscribe((action) => action());
 
-    //     expectObservable(state$).toBe('a(bcdef) 243ms g 49ms h 49ms i 49ms (jk)', {
-    //       a: {},
-    //       b: {},
-    //       c: {
-    //         root: { pending: true, asyncValidateInProgress: { 0: true } },
-    //       },
-    //       d: { emergencyContacts: { pending: true, asyncValidateInProgress: { 0: true } } },
-    //       e: { 'emergencyContacts.0': { pending: true, asyncValidateInProgress: { 0: true } } },
-    //       f: {
-    //         'emergencyContacts.0.email': {
-    //           pending: true,
-    //           asyncValidateInProgress: { 0: true, 1: true },
-    //         },
-    //       },
-    //       g: {
-    //         root: { pending: true },
-    //         emergencyContacts: { pending: true },
-    //         'emergencyContacts.0': { pending: true },
-    //         'emergencyContacts.0.email': {
-    //           pending: true,
-    //           asyncValidateInProgress: { 0: false, 1: true },
-    //           asyncValidatorErrors: { uniqueEmail: true },
-    //         },
-    //       },
-    //       h: {
-    //         root: { pending: true },
-    //         emergencyContacts: { pending: true },
-    //         'emergencyContacts.0': { pending: true },
-    //         'emergencyContacts.0.email': {
-    //           pending: false,
-    //           asyncValidateInProgress: { 0: false, 1: false },
-    //           asyncValidatorErrors: {
-    //             uniqueEmail: true,
-    //             blacklistedEmail: true,
-    //           },
-    //         },
-    //       },
-    //       i: {
-    //         emergencyContacts: {
-    //           pending: true,
-    //           asyncValidateInProgress: { 0: false },
-    //           asyncValidatorErrors: { arrayLengthError: true },
-    //         },
-    //       },
-    //       j: {
-    //         root: {
-    //           pending: true,
-    //           asyncValidateInProgress: { 0: false },
-    //           asyncValidatorErrors: { uniqueFirstAndLastName: true },
-    //         },
-    //       },
-    //       k: {
-    //         root: { pending: false },
-    //         'emergencyContacts.0': {
-    //           pending: false,
-    //           asyncValidateInProgress: { 0: false },
-    //           asyncValidatorErrors: { uniqueFirstAndLastName: true },
-    //         },
-    //       },
-    //     });
-    //   });
-    // });
+        expectObservable(state$).toBe(
+          'a(bcdef) 243ms g 49ms h 49ms i 49ms (jk) 97ms (lmnop) 243ms q 49ms r 49ms s 49ms (tu)',
+          {
+            a: {},
+            b: {},
+            c: {
+              root: { pending: true, asyncValidateInProgress: { 0: true } },
+            },
+            d: { emergencyContacts: { pending: true, asyncValidateInProgress: { 0: true } } },
+            e: { 'emergencyContacts.0': { pending: true, asyncValidateInProgress: { 0: true } } },
+            f: {
+              'emergencyContacts.0.email': {
+                pending: true,
+                asyncValidateInProgress: { 0: true, 1: true },
+              },
+            },
+            g: {
+              root: { pending: true },
+              emergencyContacts: { pending: true },
+              'emergencyContacts.0': { pending: true },
+              'emergencyContacts.0.email': {
+                pending: true,
+                asyncValidateInProgress: { 0: false, 1: true },
+                asyncValidatorErrors: { uniqueEmail: true },
+              },
+            },
+            h: {
+              root: { pending: true },
+              emergencyContacts: { pending: true },
+              'emergencyContacts.0': { pending: true },
+              'emergencyContacts.0.email': {
+                pending: false,
+                asyncValidateInProgress: { 0: false, 1: false },
+                asyncValidatorErrors: {
+                  uniqueEmail: true,
+                  blacklistedEmail: true,
+                },
+              },
+            },
+            i: {
+              emergencyContacts: {
+                pending: true,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { arrayLengthError: true },
+              },
+            },
+            j: {
+              root: {
+                pending: true,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { uniqueFirstAndLastName: true },
+              },
+            },
+            k: {
+              root: { pending: false },
+              'emergencyContacts.0': {
+                pending: false,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { uniqueFirstAndLastName: true },
+              },
+            },
+
+            l: {},
+            m: {
+              root: { pending: true, asyncValidateInProgress: { 0: true } },
+            },
+            n: { emergencyContacts: { pending: true, asyncValidateInProgress: { 0: true } } },
+            o: { 'emergencyContacts.0': { pending: true, asyncValidateInProgress: { 0: true } } },
+            p: {
+              'emergencyContacts.0.email': {
+                pending: true,
+                asyncValidateInProgress: { 0: true, 1: true },
+              },
+            },
+            q: {
+              root: { pending: true },
+              emergencyContacts: { pending: true },
+              'emergencyContacts.0': { pending: true },
+              'emergencyContacts.0.email': {
+                pending: true,
+                asyncValidateInProgress: { 0: false, 1: true },
+                asyncValidatorErrors: { uniqueEmail: true },
+              },
+            },
+            r: {
+              root: { pending: true },
+              emergencyContacts: { pending: true },
+              'emergencyContacts.0': { pending: true },
+              'emergencyContacts.0.email': {
+                pending: false,
+                asyncValidateInProgress: { 0: false, 1: false },
+                asyncValidatorErrors: {
+                  uniqueEmail: true,
+                  blacklistedEmail: true,
+                },
+              },
+            },
+            s: {
+              emergencyContacts: {
+                pending: true,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { arrayLengthError: true },
+              },
+            },
+            t: {
+              root: {
+                pending: true,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { uniqueFirstAndLastName: true },
+              },
+            },
+            u: {
+              root: { pending: false },
+              'emergencyContacts.0': {
+                pending: false,
+                asyncValidateInProgress: { 0: false },
+                asyncValidatorErrors: { uniqueFirstAndLastName: true },
+              },
+            },
+          },
+        );
+      });
+    });
   });
 });
