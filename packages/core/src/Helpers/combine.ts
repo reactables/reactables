8a44e84b93c0f0ecf5864c4b89738c59c821b75a
David Lai <laidav@gmail.com>
2025-01-04 10:13:28 -0500

    include actions observable

--- packages/core/src/Helpers/combine.ts
+++ packages/core/src/Helpers/combine.ts
@@ -1,12 +1,11 @@
-import { combineLatest, Observable } from 'rxjs';
-import { Reactable } from '../Models';
-import { RxBuilder } from './RxBuilder';
+import { combineLatest, Observable, merge, ObservedValueOf } from 'rxjs';
+import { Action, Reactable } from '../Models';
 
 export const combine = <T extends Record<string, Reactable<unknown, unknown>>>(
   reactablesDict: T,
 ) => {
-  const { states, actions } = Object.entries(reactablesDict).reduce(
-    (acc, [key, [state$, actions]]) => {
+  const { states, actions, actions$ } = Object.entries(reactablesDict).reduce(
+    (acc, [key, [state$, actions, actions$]]) => {
       return {
         states: {
           ...acc.states,
@@ -16,14 +15,24 @@ export const combine = <T extends Record<string, Reactable<unknown, unknown>>>(
           ...acc.actions,
           [key as keyof T]: actions as { [K in keyof T]: T[K][1] },
         },
+        actions$: actions$ ? acc.actions$.concat(actions$) : acc.actions$,
       };
     },
     {
       states: {} as { [K in keyof T]: Observable<T[K][0]> },
       actions: {} as { [K in keyof T]: T[K][1] },
+      actions$: [] as Observable<Action<unknown>>[],
     },
   );
   const states$ = combineLatest(states);
 
-  return [states$, actions];
+  return [states$, actions, merge(...actions$)] as [
+    Observable<{
+      [K in keyof { [K in keyof T]: Observable<T[K][0]> }]: ObservedValueOf<
+        { [K in keyof T]: Observable<T[K][0]> }[K]
+      >;
+    }>,
+    { [K in keyof T]: T[K][1] },
+    Observable<Action<unknown>>,
+  ];
 };
