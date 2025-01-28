5dc0ae87d95412f5bbf29e8968058ef2bac3e5ab
David Lai <laidav@gmail.com>
2025-01-28 09:49:30 -0500

    [26] web worker integration

--- packages/core/src/Helpers/storeValue.ts
+++ packages/core/src/Helpers/storeValue.ts
@@ -5,14 +5,21 @@ export interface DestroyAction {
   destroy: () => void;
 }
 
-export const storeValue = <T, S>(reactable: Reactable<T, S>): Reactable<T, S & DestroyAction> => {
+export const storeValue = <T, S>(
+  reactable: Reactable<T, S & { destroy?: () => void }>,
+): Reactable<T, S & DestroyAction> => {
   const replaySubject$ = new ReplaySubject<T>(1);
 
   const [state$, actions, actions$] = reactable;
 
   const subscription = state$.subscribe((state) => replaySubject$.next(state));
 
-  const destroy = () => subscription.unsubscribe();
+  const destroy = () => {
+    // If destroy action already exists (i.e from a web worker), invoke it
+    actions.destroy && actions.destroy();
+
+    subscription.unsubscribe();
+  };
 
   return [replaySubject$, { ...actions, destroy }, actions$];
 };
