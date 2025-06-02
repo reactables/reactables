37ba9e3490204e23de918216988f8ad00c41abd5
David Lai <laidav@gmail.com>
2025-06-01 20:30:58 -0400

    update interface for storevalue

--- packages/core/src/Helpers/storeValue.ts
+++ packages/core/src/Helpers/storeValue.ts
@@ -5,7 +5,9 @@ export interface DestroyAction {
   destroy: () => void;
 }
 
-export const storeValue = <T, S>(reactable: Reactable<T, S>): Reactable<T, S & DestroyAction> => {
+export const storeValue = <T, S, U extends Record<string, string> = Record<string, string>>(
+  reactable: Reactable<T, S, U>,
+): Reactable<T, S & DestroyAction, U> => {
   const replaySubject$ = new ReplaySubject<T>(1);
 
   const [state$, actions, actions$] = reactable;
