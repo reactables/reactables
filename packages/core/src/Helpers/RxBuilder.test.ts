90f251e5394aaf11619fb512d212d8496f7db93c
David Lai <laidav@gmail.com>
2025-06-04 12:26:08 -0400

    update test

--- packages/core/src/Helpers/RxBuilder.test.ts
+++ packages/core/src/Helpers/RxBuilder.test.ts
@@ -36,6 +36,12 @@ const RxCounter = <T extends Cases<CounterState>>(reducers?: T) =>
     },
   });
 
+const RxCombined = () =>
+  combine({
+    counter: RxCounter({ extendedCounter: (state) => state }),
+    toggle: RxToggle(),
+  });
+
 describe('RxBuilder', () => {
   it('should generate types for reactables', () => {
     const [, , counterActions$] = RxCounter();
@@ -74,6 +80,47 @@ describe('RxBuilder', () => {
       hi: 'hi',
       'some wierd reducer': 'some wierd reducer',
       extendedCounter: 'extendedCounter',
+    });
+  });
+
+  it('should generate types for combined reactables', () => {
+    const [, , actions$] = RxCombined();
+
+    expect(actions$.types).toEqual({
+      '[counter] - extendedCounter': '[counter] - extendedCounter',
+      '[counter] - increment': '[counter] - increment',
+      '[counter] - setCounter': '[counter] - setCounter',
+      '[counter] - hi': '[counter] - hi',
+      '[counter] - some wierd reducer': '[counter] - some wierd reducer',
+      '[toggle] - toggle': '[toggle] - toggle',
+      '[toggle] - toggleOn': '[toggle] - toggleOn',
+      '[toggle] - toggleOff': '[toggle] - toggleOff',
+      '[toggle] - setToggle': '[toggle] - setToggle',
+    });
+  });
+
+  it('should generate types for reactables combined multiple times', () => {
+    const [, , actions$] = combine({
+      combined: RxCombined(),
+      multiplier: RxBuilder({
+        initialState: 0,
+        reducers: {
+          multiply: (state, action: Action<number>) => state * action.payload,
+        },
+      }),
+    });
+
+    expect(actions$.types).toEqual({
+      '[combined] - [counter] - extendedCounter': '[combined] - [counter] - extendedCounter',
+      '[combined] - [counter] - increment': '[combined] - [counter] - increment',
+      '[combined] - [counter] - setCounter': '[combined] - [counter] - setCounter',
+      '[combined] - [counter] - hi': '[combined] - [counter] - hi',
+      '[combined] - [counter] - some wierd reducer': '[combined] - [counter] - some wierd reducer',
+      '[combined] - [toggle] - toggle': '[combined] - [toggle] - toggle',
+      '[combined] - [toggle] - toggleOn': '[combined] - [toggle] - toggleOn',
+      '[combined] - [toggle] - toggleOff': '[combined] - [toggle] - toggleOff',
+      '[combined] - [toggle] - setToggle': '[combined] - [toggle] - setToggle',
+      '[multiplier] - multiply': '[multiplier] - multiply',
     });
   });
 });
