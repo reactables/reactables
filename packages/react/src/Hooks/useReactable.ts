6a9484f8d9b557819b999812f83f53fc245797c9
David Lai <laidav@gmail.com>
2025-09-01 12:43:00 -0400

    fix useReactable for strict mode

--- packages/react/src/Hooks/useReactable.ts
+++ packages/react/src/Hooks/useReactable.ts
@@ -16,6 +16,7 @@ export const useReactable = <
   ...props: U
 ): HookedReactable<typeof reactableFactory> => {
   const rx = useRef<Reactable<T, S, V>>(null);
+  const lastMount = useRef<Date>(null);
 
   /**
    * React Strict Mode has bugs with clean up with refs so it breaks the useReactable hook as of now
@@ -31,13 +32,20 @@ export const useReactable = <
   const [state, setState] = useState<T>();
 
   useEffect(() => {
+    lastMount.current = new Date();
+    console.log(lastMount.current.getTime());
     const subscription = state$.subscribe((result) => {
       setState(result);
     });
 
     return () => {
-      actions.destroy?.();
+      console.log('unsubscribe');
       subscription.unsubscribe();
+
+      if (new Date().getTime() - lastMount.current.getTime() > 10) {
+        console.log('destroy');
+        actions.destroy?.();
+      }
     };
   }, [actions, state$]);
 
